3644dcdd038c7b6a3bb5098518ced8b50229ff31
Nicolas Dufreche <n.dufreche@gmail.com>
2020-09-18 17:15:30 +0200

    creating action

--- src/action/lib.js
+++ src/action/lib.js
@@ -1,3 +1,5 @@
 export const ADD = Symbol('@LIB/ADD')
+export const add = (name, doc) => ({ type: ADD, payload: { name, doc } })
 
 export const REMOVE = Symbol('@LIB/REMOVE')
+export const remove = (name) => ({ type: REMOVE, payload: { name } })
--- src/action/stack.js
+++ src/action/stack.js
@@ -1,5 +1,7 @@
 export const CREATE = Symbol('@LIB/NEW')
+export const createStack = (name, type) => ({ type: CREATE, payload: { name, type }})
 
 export const EDIT = Symbol('@LIB/EDIT')
 
 export const REMOVE = Symbol('@LIB/REMOVE')
+export const remove = (name) => ({ type: REMOVE, payload: { name }})
