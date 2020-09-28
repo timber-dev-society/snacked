183bed8a68b498fac408148009d498ca034828f8
jius <jius.dev@gmail.com>
2020-09-28 15:29:07 +0200

    Update Card title

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,27 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import List from '../list/List'
 import { IoMdAdd } from 'react-icons/io'
 import { FiEdit2 } from 'react-icons/fi'
+import { BsCheckCircle} from 'react-icons/bs'
 
-const Card = ({card, setModalShow, setSelectedResource}) => (
-  <div className="max-w-md rounded overflow-hidden shadow-lg mx-auto md:mx-none">
-    <div className="px-6 py-4">
-      <div className="flex justify-center items-center">
-        <div className="font-bold text-xl text-teal-700">{card.title}</div>
-        <a href="#" className="ml-1 text-gray-600 hover:text-gray-900 hover:text-teal-500"><FiEdit2/></a>
-      </div>
+const Card = ({card, setModalShow, setSelectedResource}) => {
+  return (
+    <div className="max-w-md rounded overflow-hidden shadow-lg mx-auto md:mx-none">
+      <div className="px-6 py-4">
+        <CardTitle card={card} />
 
-      <div className="resources">
-        {card.resources.map( resource => <List resource={resource} setModalShow={setModalShow} setSelectedResource={setSelectedResource} /> )}
-      </div>
+        <div className="resources">
+          {card.resources.map( resource => <List resource={resource} setModalShow={setModalShow} setSelectedResource={setSelectedResource} /> )}
+        </div>
 
-      <div className="text-center my-2">
-        <button class="bg-transparent hover:bg-teal-700 border border-teal-700 hover:border-transparent text-teal-700 hover:text-white font-bold p-4 rounded-full">
-          <IoMdAdd/>
-        </button>
+        <div className="text-center my-2">
+          <button class="bg-transparent hover:bg-teal-700 border border-teal-700 hover:border-transparent text-teal-700 hover:text-white font-bold p-4 rounded-full">
+            <IoMdAdd/>
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+}
+
+const CardTitle = ({card}) => {
+  const [title, setTitle] = useState(card.title)
+  const [editTitle, setEditTitle] = useState(false)
+
+  if ( !editTitle ) {
+    return (
+      <div className="card-title flex justify-center items-center">
+        <div className="font-bold text-xl text-teal-700">{title}</div>
+        <button 
+          className="ml-1 text-gray-600 hover:text-gray-900 hover:text-teal-500"
+          onClick={() => {
+            setEditTitle(true)
+          }}
+        >
+          <FiEdit2/>
+        </button>
+      </div>
+    )
+  } else {
+    return (
+      <div className="card-title flex justify-center items-center">
+        <input
+          type="text"
+          value={title}
+          className="font-bold text-xl text-teal-700 text-center shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+          onChange={ (e) => {
+            setTitle(e.target.value)
+          }}/>
+
+        <button 
+          className="ml-1 text-gray-600 hover:text-gray-900 hover:text-teal-500 text-xl"
+          onClick={() => {
+            setEditTitle(false)
+          }}
+        >
+          <BsCheckCircle/>
+        </button>
+      </div>
+    )
+  }
+}
+
 
 export default Card;
