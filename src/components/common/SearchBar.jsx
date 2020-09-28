1ab35cba3a9212d6b57f9ea121319bb2781af81e
jius <jius.dev@gmail.com>
2020-09-28 23:36:01 +0200

    Update tags with localStorage

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BiSearchAlt } from 'react-icons/bi'
-import { VscClose } from 'react-icons/vsc'
 import Tag from '../tag/Tag'
+import { useLocalStorage } from '../../hooks/localStorage'
 
-export default () => (
-  <div className="searchbar my-3">
-    <div className="relative">
-      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-        <span className="text-gray-500 text-2xl">
-          <BiSearchAlt/>
-        </span>
+export default () => {
+  const [tags, setTags] = useLocalStorage('tags', ['react', 'rubyonrails', 'awesome'])
+
+  return (
+    <div className="searchbar my-3">
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <span className="text-gray-500 text-2xl">
+            <BiSearchAlt/>
+          </span>
+        </div>
+        <input className="text-2xl shadow appearance-none border pl-10 p-3 rounded w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="q" type="text" placeholder="Recherche" />
       </div>
-      <input className="text-2xl shadow appearance-none border pl-10 p-3 rounded w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="q" type="text" placeholder="Recherche" />
-    </div>
 
-    <div className="flex items-center my-2">
-      <Tag name="react" />
-      <Tag name="rubyonrails" />
-      <Tag name="awesome" />
+      <div className="flex items-center my-2">
+        {tags.map(tagName => <Tag tagname={tagName} setTags={setTags} tags={tags} />)}
+      </div>
     </div>
-  </div>
-);
+  )
+}
--- src/components/tag/Tag.jsx
+++ src/components/tag/Tag.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { VscClose } from 'react-icons/vsc'
 
-export default ({name}) => (
-  <div className="flex items-center bg-gray-200 rounded-full px-3 py-1 mr-2 mb-2">
-    <div className="text-sm font-semibold text-gray-700 ">#{name}</div>
-    <button><VscClose/></button>
-  </div>
-)
+export default ({tagname, tags, setTags}) => {
+  return (
+    <div className="flex items-center bg-gray-200 rounded-full px-3 py-1 mr-2 mb-2">
+      <div className="text-sm font-semibold text-gray-700 ">#{tagname}</div>
+      <button onClick={ () => {
+        setTags(tags.filter(e => e !== tagname))
+      }}>
+        <VscClose/></button>
+    </div>
+  )
+}
